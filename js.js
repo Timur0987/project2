72500b54f40e8c1c137113582fe0e9ee1ef389e4
Timur <tima.trigubov@icloud.com>
2021-10-02 19:02:50 +0300

    section 2 lessons 22

--- js.js
+++ js.js
@@ -1,60 +1,92 @@
-const numberOfFilms = +prompt('скольок фильмов вы уже посмотрели', '');
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
+// const numberOfFilms = +prompt('скольок фильмов вы уже посмотрели', '');
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// };
 
-for (let i = 0; i < 2; i++) {
-    const a = prompt('какой последний фильм ты смотрел', ''),
-        b = prompt('на сколько вы его оцениваете', '');
+// for (let i = 0; i < 2; i++) {
+//     const a = prompt('какой последний фильм ты смотрел', ''),
+//         b = prompt('на сколько вы его оцениваете', '');
 
-    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-        personalMovieDB.movies[a] = b;
-        console.log('done');
-    } else {
-        console.log('error');
-        i--;
-    }
+//     if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+//         personalMovieDB.movies[a] = b;
+//         console.log('done');
+//     } else {
+//         console.log('error');
+//         i--;
+//     }
 
+// }
+
+// if (personalMovieDB.count < 10) {
+//     console.log('посмотрел мало');
+// } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+//     console.log('классический зриетль');
+// } else if (personalMovieDB.count >= 30) {
+//     console.log('вы киноман');
+// } else {
+//     console.log('error');
+// }
+
+// console.log(personalMovieDB);
+
+// const options = {
+//     name: 'test',
+//     width: 1024,
+//     height: 1024,
+//     colors: {
+//         border: 'black',
+//         bg: 'green'
+//     },
+//     makeTest: function () {
+//         console.log('Test');
+//     }
+// }
+// options.makeTest()
+
+// const {border, bg} = options.colors
+
+// console.log(Object.keys(options).length)
+
+// // console.log(options.colors)
+
+// // delete options.name
+
+// // console.log(options);
+// let counter = 0
+// for (let key in options) {
+//     if (typeof (options[key]) === 'object') {
+//         for (let i in options[key]) {
+//             console.log(`свойство ${i} имеет значение ${options[key][i]}`)
+//             counter++
+//         }
+//     } else {
+//         console.log(`свойство ${key} имеет значение ${options[key]}`)
+//         counter++
+//     }
+// }
+// 
+
+const arr = [1,2,3,4,5]
+
+arr.pop()//delete last object
+arr.push(10)// add object  last 
+
+for (let i = 0; i < arr.length;i++){
+    console.log(arr[i])
 }
 
-if (personalMovieDB.count < 10) {
-    console.log('посмотрел мало');
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-    console.log('классический зриетль');
-} else if (personalMovieDB.count >= 30) {
-    console.log('вы киноман');
-} else {
-    console.log('error');
-}
+for (let value of arr){
+    console.log(value)
+}//'of' work only in array
 
-console.log(personalMovieDB);
+arr.forEach(function(item,i,arr){
+    console.log(`${i}: ${item} inside an array ${arr} `)
+})
 
-const options = {
-    name: 'test',
-    width: 1024,
-    height: 1024,
-    colors: {
-        border: 'black',
-        bg: 'green'
-    }
-}
-
-// console.log(options.colors)
-
-// delete options.name
-
-// console.log(options);
-
-for (let key in options) {
-    if (typeof (options[key]) === 'object') {
-        for (let i in options[key]) {
-            console.log(`свойство ${i} имеет значение ${options[key][i]}`)
-        }
-    } else{
-        console.log(`свойство ${key} имеет значение ${options[key]}`)
-    }
-}
+const str = prompt('','')
+const products = str.split('', '')
+console.log(products.join('; '))
