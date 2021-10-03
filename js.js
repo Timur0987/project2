3c11bb5276e8f77a437c61f41a262af4a30ebfdc
Timur <tima.trigubov@icloud.com>
2021-10-03 20:03:25 +0300

    spread section 2 lessons 22

--- js.js
+++ js.js
@@ -94,3 +94,37 @@ arr.forEach(function (item, i, arr) {
 const str = prompt('', '')
 const products = str.split('', '')
 console.log(products.join('; '))
+
+
+const obj = {
+    a: 5,
+    b: 1
+}
+
+// const copy  = obj
+// copy.a= 10
+
+function copy(mainObj) {
+    let objCopy = {}
+    let key;
+    for (key in mainObj) {
+        objCopy[key] = mainObj[key]
+    }
+    return objCopy
+}
+
+const numbers = {
+    a: 2,
+    b: 5,
+    c: {
+        x: 7,
+        y: 4
+    }
+}
+
+const newNumbers = copy(numbers)
+
+newNumbers.a = 10
+
+console.log(newNumbers)
+console.log(numbers)
